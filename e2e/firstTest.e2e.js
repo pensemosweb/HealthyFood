62887ef1d77478fbf3e438cbf461d21587d0dcff
Jaime Cervantes Velasco <jaime.cervantes.ve@gmail.com>
2022-07-05 12:30:06 -0500

    chore: installing detox for e2e testing

--- e2e/firstTest.e2e.js
+++ e2e/firstTest.e2e.js
@@ -1,14 +1,20 @@
 describe('Given an app which collect all the healthy food of Tezonapa', () => {
+  beforeAll(async () => {
+    await device.launchApp();
+  });
+
+  beforeEach(async () => {
+    await device.reloadReactNative();
+  });
+
   describe('When an user wants to find a healthier food', () => {
     it('Then a list of healthy food should be shown', () => {
-      await device.reloadReactNative();
       await element(by.id('post')).toBeVisible()
     });
   });
 
   describe('When a user selects a healthy to see more information', () => {
     it('Then more information should be shown in the screen', () => {
-      await device.reloadReactNative();
       await element(by.id('more-info')).tap();
       await element(by.id('post-details')).toBeVisible();
     });
